559d4aeba2a3e101a2540685943416c2afc5b279
Joshhshh <137018415+4evergr8@users.noreply.github.com>
2025-03-03 21:31:07 +0800

    Add file: \程序\Music\Justin Bieber\Believe (Deluxe Edition) (iTunes + LP)\Believe (Deluxe Edition).itlp\controllers\data.js

---
+++ Justin Bieber/Believe (Deluxe Edition) (iTunes + LP)/Believe (Deluxe Edition).itlp/controllers/data.js
@@ -0,0 +1,24 @@
+var appData = {
+  feature : { XID : "TEST:uuid:B5ADF718-D4C8-473F-BFAF-B86570A233DF", title : "Believe", artist : "Justin Bieber" },
+  audioLoop : { src : "audio/intro.m4a", loop : false },
+  numberOfPhotos : 11,
+  songs : [
+    { XID : "Universal:isrc:USUM71205293" },
+    { XID : "Universal:isrc:USUM71202650" },
+    { XID : "Universal:isrc:USUM71205320" },
+    { XID : "Universal:isrc:USUM71205333" },
+    { XID : "Universal:isrc:USUM71205350" },
+    { XID : "Universal:isrc:USUM71205295" },
+    { XID : "Universal:isrc:USUM71205292" },
+    { XID : "Universal:isrc:USUM71205353" },
+    { XID : "Universal:isrc:USUM71205298" },
+    { XID : "Universal:isrc:USUM71205367"},
+    { XID : "Universal:isrc:USUM71205299" },
+    { XID : "Universal:isrc:USUM71205315" },
+    { XID : "Universal:isrc:USUM71205368" },
+    { XID : "Universal:isrc:USUM71205369" },
+    { XID : "Universal:isrc:USUM71205374" },
+    { XID : "Universal:isrc:USUM71205389" },
+    { XID : "Universal:isrc:USUM71205410" }
+  ],
+};
